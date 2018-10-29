44267dd6ba5513dcf640842b2f13cbe420fc7393
Steven Vi <visteven00@gmail.com>
2018-10-30 09:30:53 +1100

    add enum to user authentication table

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum type_of_user: {'customer':0 'chef':1}
 end
