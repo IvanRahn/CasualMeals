4d7e0f90d10866d5bdb940c6dc0b1b294acbbbbc
Ivan <ivan.rahn@gmail.com>
2018-10-30 21:35:50 +1100

    remove customer access to edit/destroy meals

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resources :meals
-  #devise_for :users
-  devise_for :users, path: "", path_names: {sign_in: "login", sign_out: "logout", sign_up: "register"}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root "home#welcome"
+  devise_for :users, path: "", path_names: {sign_in: "login", sign_out: "logout", sign_up: "register"}
   resources :chefs
+  resources :meals
 end
