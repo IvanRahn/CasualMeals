fc00ca680f424f6e79ffab3171cd6a7a9e1e71bd
Ivan <ivan.rahn@gmail.com>
2018-10-30 15:59:18 +1100

    add permission to view by role

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -1,5 +1,5 @@
 class MealsController < ApplicationController
-  #authenticate that the user is a chef  
+  #authenticate that the user is a chef
   #authenticate that the user is a chef
   #authenticate that the user is a chef in order to allow access to CRUD actions and their specific dishes, all chefs can view each other's meals
   #customer should only have show access
@@ -9,7 +9,13 @@ class MealsController < ApplicationController
   # GET /meals
   # GET /meals.json
   def index
-    @meals = Meal.all
+    @meals =
+      case current_user.type_of_user
+      when "chef"
+        Meal.where(chef_id: current_user.chef.id)
+      when "customer"
+        Meal.all
+      end
   end
 
   # GET /meals/1
@@ -33,7 +39,7 @@ class MealsController < ApplicationController
     @meal.chef_id = current_user.chef.id
     respond_to do |format|
       if @meal.save
-        format.html { redirect_to @meal, notice: 'Meal was successfully created.' }
+        format.html { redirect_to @meal, notice: "Meal was successfully created." }
         format.html { redirect_to @meal, notice: "Meal was successfully created." }
         format.json { render :show, status: :created, location: @meal }
       else
@@ -48,7 +54,7 @@ class MealsController < ApplicationController
   def update
     respond_to do |format|
       if @meal.update(meal_params)
-        format.html { redirect_to @meal, notice: 'Meal was successfully updated.' }
+        format.html { redirect_to @meal, notice: "Meal was successfully updated." }
         format.html { redirect_to @meal, notice: "Meal was successfully updated." }
         format.json { render :show, status: :ok, location: @meal }
       else
@@ -63,7 +69,7 @@ class MealsController < ApplicationController
   def destroy
     @meal.destroy
     respond_to do |format|
-      format.html { redirect_to meals_url, notice: 'Meal was successfully destroyed.' }
+      format.html { redirect_to meals_url, notice: "Meal was successfully destroyed." }
       format.html { redirect_to meals_url, notice: "Meal was successfully destroyed." }
       format.json { head :no_content }
     end
