0a541dd45ec78076701f0e79a8ef0314445f6ad8
Ivan <ivan.rahn@gmail.com>
2018-10-30 14:56:27 +1100

    fix login

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -9,9 +9,9 @@ class HomeController < ApplicationController
   def check_signed_in
     if user_signed_in? and current_user.type_of_user == "customer"
       redirect_to meals_path
-    elsif user_signed_in? and current_user.type_of_user == "chef" and !current_user.chef?
+    elsif user_signed_in? and current_user.type_of_user == "chef" and !current_user.chef
         redirect_to new_chef_path
-   elsif user_signed_in? and current_user.type_of_user == "chef" and current_user.chef?
+   elsif user_signed_in? and current_user.type_of_user == "chef" and current_user.chef
          redirect_to meals_path
     end
   end
