83c9f7011e5b01343121695d519498340d679202
Steven Vi <visteven00@gmail.com>
2018-10-31 13:07:09 +1100

    add transaction controller

--- app/controllers/transaction_contoller.rb
+++ app/controllers/transaction_contoller.rb
@@ -1,6 +1,17 @@
 class TransactionController < ApplicationController
-    def home 
+    def index 
         @transactions = Transaction.all
     end
 
+    def create
+        @transactions = Transaction.new(transaction_params)
+        @transactions.user_id = current_user.user.id
+        respond_to do |format|
+          if @transaction.save
+            format.html { redirect_to @transactions, notice: "Transaction created." }
+          else
+            format.html { render :new }
+          end
+        end
+      end
 end
