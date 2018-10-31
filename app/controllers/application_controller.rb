660548e47c219f1f2ee44916e5f663877a25c81a
Ivan <ivan.rahn@gmail.com>
2018-10-31 12:51:29 +1100

    add foundation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
-    before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :authenticate_user!, if: proc {|c| params[:controller] != "home"}
+  before_action :reroute
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
+
+  def reroute
+if user_signed_in? 
+
+end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name, :last_name, :phone_number, :address, :type_of_user])
