2c236fe9a616e27a4113893b04b4d587241755ed
Ivan <ivan.rahn@gmail.com>
2018-11-03 19:16:53 +1100

    add name to meals

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_01_093022) do
+ActiveRecord::Schema.define(version: 2018_11_03_080846) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2018_11_01_093022) do
     t.bigint "chef_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
     t.index ["chef_id"], name: "index_meals_on_chef_id"
   end
 
