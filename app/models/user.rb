80ce56ce825b71ee1af16f256393a1528436778a
Ivan <ivan.rahn@gmail.com>
2018-11-06 15:47:48 +1100

    add eager load

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, presence: true, format: {with: /\A[a-zA-Z]+\z/,
                                                               message: "only allows letters"}
-  validates :phone_number, format: {with: /\A\d{10,14}\z/,
+  validates :phone_number, format: {with: /\A+?\d{10,16}\z/,
                                     message: "needs to be 10 or more digits"}
   validates :address, presence: true
   enum type_of_user: {'customer': 0, 'chef': 1}
