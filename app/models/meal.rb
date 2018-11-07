2a8d5fe85b7b7cab470d8305cb3f9c7039371d9d
Ivan <ivan.rahn@gmail.com>
2018-11-07 11:02:33 +1100

    add validations

--- app/models/meal.rb
+++ app/models/meal.rb
@@ -1,15 +1,25 @@
 class Meal < ApplicationRecord
+  #associations
   belongs_to :chef
   has_many :meal_transactions
   has_many :orders, through: :meal_transactions
+  # carrierwave
   mount_uploader :image, MealImageUploader
+  #validations
+  # validate cuisine
+  validates :cuisine, inclusion: {in: %w(chinese french japanese vietnamese turkish american others),
+                          message: "%{value} is not a cuisine"}
+  # validate description
+  validates :description, length: {minimum: 25}
+  # validate price
+  validates :price, format: {with: /\A\d{0,8}\z/}
+  # validates name, image
+  validates :name, :image, presence: true
+
+  # concat chef's first and last name
 
   def chef_name
     user = self.chef.user
     "#{user.first_name} #{user.last_name}"
   end
-
-  # def self.cuisine_type
-  #   self.distinct.pluck(:cuisine)
-  # end
 end
